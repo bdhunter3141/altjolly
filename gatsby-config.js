2609397353213896ba0259f5b06a902193dba9fc
Bryan Hunter <bdhunter3141@gmail.com>
2020-07-25 18:28:03 -0400

    refactor: more configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `AltJolly`,
+    description: `For the love of plants.`,
+    author: `Bryan Hunter`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
